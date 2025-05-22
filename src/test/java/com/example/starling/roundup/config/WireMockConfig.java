6400434d9c6cce38e3d3bcb9e61c8a4bd044b74a
Kevin Lee <cameljava@gmail.com>
2025-05-22 13:31:43 +1000

    refactor and fix compose and perfomance test

--- src/test/java/com/example/starling/roundup/config/WireMockConfig.java
+++ src/test/java/com/example/starling/roundup/config/WireMockConfig.java
@@ -1,21 +1,14 @@
 package com.example.starling.roundup.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.contract.wiremock.WireMockConfigurationCustomizer;
-
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class WireMockConfig {
 
     @Bean
     public WireMockConfigurationCustomizer wireMockConfigurationCustomizer() {
-        return new WireMockConfigurationCustomizer() {
-            @Override
-            public void customize(WireMockConfiguration config) {
-                config.usingFilesUnderDirectory("src/test/resources/wiremock");
-            }
-        };
+        return config -> config.usingFilesUnderDirectory("src/test/resources/wiremock");
     }
 } 
